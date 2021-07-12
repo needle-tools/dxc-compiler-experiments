e7460055d8a1765d4cb36962d66472766d015097
Felix Herbst <felix.herbst@gmail.com>
2021-07-12 13:37:35 +0200

    more experiments with RTX + Particles

--- 2021.2-HDRP/Assets/BakeParticleMesh.cs
+++ 2021.2-HDRP/Assets/BakeParticleMesh.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,17 +8,45 @@ using UnityEngine.Experimental.Rendering;
 public class BakeParticleMesh : MonoBehaviour
 {
     public ParticleSystemRenderer target;
-    private Mesh mesh;
+    
+    private static readonly Dictionary<ParticleSystemRenderer, (int lastFrame, Mesh cachedMesh)> cache = new Dictionary<ParticleSystemRenderer, (int, Mesh)>();
+    // private Mesh mesh;
+
+    private int lastFrame = -1;
+
+    private MeshFilter meshFilter;
+    private MeshRenderer meshRenderer;
     
     void Update()
     {
         if (!target) return;
-        
-        if (mesh == null)
-            mesh = new Mesh();
 
-        target.BakeMesh(mesh, Camera.main, true);
-        GetComponent<MeshFilter>().sharedMesh = mesh;
-        GetComponent<MeshRenderer>().rayTracingMode = RayTracingMode.DynamicGeometry;
+        if (!cache.ContainsKey(target))
+        {
+            var mesh = new Mesh {hideFlags = HideFlags.DontSave};
+            cache.Add(target, (-1, mesh));
+        }
+        
+        if(cache.TryGetValue(target, out var thing))
+        {
+            if (thing.lastFrame < Time.renderedFrameCount)
+            {
+                var mesh = thing.cachedMesh;
+                target.BakeMesh(mesh, Camera.main, true);
+                thing.lastFrame = Time.renderedFrameCount;
+                thing.cachedMesh = mesh;
+                cache[target] = thing;
+            }
+            
+            if(!meshFilter) meshFilter = GetComponent<MeshFilter>();
+            if(!meshRenderer) meshRenderer = GetComponent<MeshRenderer>();
+            
+            meshFilter.sharedMesh = thing.cachedMesh;
+            meshRenderer.rayTracingMode = RayTracingMode.DynamicGeometry;
+        }
+        else
+        {
+            Debug.LogWarning("Target not in cache", target);
+        }
     }
 }
